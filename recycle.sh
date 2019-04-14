91967033645f998e7c03c02f2bca9b46090740b8
Marcus Hilaire <marcushilaire@gmail.com>
2019-04-14 19:42:53 -0400

    Scaffolding for flags in place

--- recycle.sh
+++ recycle.sh
@@ -2,21 +2,31 @@
 dump=`dirname $0`/garbage
 #Parse through arguments and handle the option flags
 while getopts ':ehxuz' OPTION; 
-do case $OPTION in
-    e) echo option selected e
-    ;;
-    h) echo option selected h
-    ;;
-    \?) echo not a valid option
-    ;;
-esac
+    do case $OPTION in
+        e) echo option selected e
+        e=true
+        ;;
+        h) echo option selected h
+        h=true
+        ;;
+        \?) echo not a valid option
+        ;;
+    esac
 done
 shift "$(($OPTIND -1))"
 
-    # echo $* #references all arguments that ARE NOT options parsed here
+main () {
+    if [ $e ]
+    then
+        echo e set to true
+    fi
+    if [ $h ]
+    then
+        echo h is set to true
+    fi
+}
+main
 
-echo $*
-echo $@
 
 #Create the garbage folder if it does not exist
     #Should also touch the Tracker file
@@ -36,10 +46,10 @@ fi
 
 #Move the listed files to the dump
 moveFile (){
-    arg1=$1
-    mv
+    mv $1 $dump
+    echo recycling $1
 }
-
+moveFile $@
 
 #List all files in the recycle bin along with their size
 listFiles(){
