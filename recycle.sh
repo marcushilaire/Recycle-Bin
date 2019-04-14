0b0b117846c671610c16c04d88b786b84b8557b9
Marcus Hilaire <marcushilaire@gmail.com>
2019-04-14 13:04:41 -0400

    Handling arguments and Starting functions

--- recycle.sh
+++ recycle.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 dump=`dirname $0`/garbage
 
@@ -17,15 +17,28 @@ fi
 
 #Take in Arugments.
 
-checkArguments (){
-echo $#
-}
-#Move the listed files to the dump
-moveFile (){
-    mv $1 $dump
+
+
+moveByArgs (){
+    #not using this but do not delete
+    #ask for a refresher on scope because i do not want to just run a for loop on moveFile
+    for i in `seq $#`
+        do 
+            echo hello $i
+        done
 }
 
+#Move the listed files to the dump
+moveFile (){
+    arg1=$1
+    mv
+}
+
+
 #List all files in the recycle bin along with their size
+listFiles(){
+    ls -sh $dump
+}
 
 #Create a file that records the original location of the file
     #Restore based on parsing throug that file
@@ -35,7 +48,6 @@ cat $dump/tracker.info |grep bye
 }  
 #
 
-
 # handle duplicate file names 
 
-checkGarbage
+# checkGarbage
