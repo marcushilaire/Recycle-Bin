9e77792b7098926c8e2e97626dbdfc031f3f227b
Marcus Hilaire <marcushilaire@gmail.com>
2019-04-14 12:11:18 -0400

    Planning next steps

--- recycle.sh
+++ recycle.sh
@@ -1,15 +1,41 @@
 #! /bin/bash
 
+dump=`dirname $0`/garbage
+
 #Create the garbage folder if it does not exist
-dump=`dirname $0`
+    #Should also touch the Tracker file
 checkGarbage (){
 if [ ! -d ./garbage ]
-then
-    mkdir $dump/garbage
-    echo garbage created
-else
-    echo garbage exists
+    then
+        mkdir $dump
+        touch $dump/tracker.info
+        echo garbage created
+    else
+        echo garbage exists
 fi
 }
+
+#Take in Arugments.
+
+checkArguments (){
+echo $#
+}
+#Move the listed files to the dump
+moveFile (){
+    mv $1 $dump
+}
+
+#List all files in the recycle bin along with their size
+
+#Create a file that records the original location of the file
+    #Restore based on parsing throug that file
+        #Cat through then pipe into a grep or awk
+trackLocation(){
+cat $dump/tracker.info |grep bye
+}  
+#
+
+
+# handle duplicate file names 
+
 checkGarbage
-echo $dump
