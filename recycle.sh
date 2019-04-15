0de7bb6ef93c89f6541d8bcdc4433c6ef46291dc
marcushilaire <marcushilaire@gmail.com>
2019-04-15 16:19:10 -0400

    Adjusting loops and implementing checkGarbage

--- recycle.sh
+++ recycle.sh
@@ -13,40 +13,32 @@ while getopts ':le' OPTION;
         ;;
     esac
 done
-
 shift "$(($OPTIND -1))"
 
 #Create the garbage folder if it does not exist
-    #Should also touch the Tracker file
 initiateGarbage (){
 if [ ! -d ./garbage ]
     then
         mkdir $dump
         touch $dump/tracker.info
         echo garbage created
-    else
-        echo garbage exists
 fi
 }
 
 #check to see if a file exists in garbage
 checkGarbage (){
-    echo checking garbage to see if a file exists
     if [[ -f $dump/$1 || -d $dump/$1 ]]
     then 
-    echo $i already exists in your Recycle Bin
-    echo please rename the file you are attempting to recycle
-    return 1
+        echo $i already exists in your Recycle Bin
+        echo please rename the file you are attempting to recycle
+        return 1
     else
-    echo proceeding
-    return 0
+        return 0
     fi
 }
 
 #Move the listed files to the dump and record their original location
 recycleFile (){
-    # for i in $@
-    # do
         if [[ -f $1 || -d $1 ]]
         then
             mv $1 $dump
@@ -55,11 +47,8 @@ recycleFile (){
         else
             echo $1 is not a valid file 
         fi
-    # done
 }
 
-# recycleFile $@
-# echo
 #List all files in the recycle bin along with their size
 listFiles(){
     ls -sh $dump
@@ -72,31 +61,19 @@ trackLocation(){
 cat $dump/tracker.info |grep bye
 }  
 #
+
 main () {
     if [[ $l && $e ]]
     then echo l and e are mutually exclusive, exiting
     exit 1
     fi
-    
-    if [ $l ]
-    then
-        echo l set to true
-    fi
-
-    if [ $e ]
-    then
-        echo e is set to true
-    fi
 
     for i in $@
     do
-        checkGarbage $i
-            # if [ checkGarbage $i ]
-            # then
-            #     echo check passed
-            # else
-            #     echo check failed
-            # fi
+        if checkGarbage $i
+        then
+            recycleFile $i
+        fi
     done
 }
 main $@
